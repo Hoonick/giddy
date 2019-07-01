b8a04be4a416242b0bdebeab59dfa2e7b8459969
chenghong <chenghong@tv189.com>
2019-07-01 15:01:14 +0800

    sssss

--- src/main/java/com/hoonick/giddy/TestGit.java
+++ src/main/java/com/hoonick/giddy/TestGit.java
@@ -3,7 +3,7 @@ package com.hoonick.giddy;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("hello local 3333344443");
+		System.out.println("hello local 333333333333333");
 	}
 
 }
