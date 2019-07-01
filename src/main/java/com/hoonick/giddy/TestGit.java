a414923c357b49af2792f1a1c5c28a681a5f284e
chenghong <chenghong@tv189.com>
2019-07-01 14:37:59 +0800

    xxx

--- src/main/java/com/hoonick/giddy/TestGit.java
+++ src/main/java/com/hoonick/giddy/TestGit.java
@@ -3,7 +3,7 @@ package com.hoonick.giddy;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("hello test conflicts cloud");
+		System.out.println("hello local");
 	}
 
 }
