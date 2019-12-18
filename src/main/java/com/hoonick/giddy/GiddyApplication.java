3858744851c5d53edcd376febe514b3e1220bb45
Hoonick <hoonick@163.com>
2019-12-18 10:07:23 +0800

    giddy

--- src/main/java/com/hoonick/giddy/GiddyApplication.java
+++ src/main/java/com/hoonick/giddy/GiddyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GiddyApplication {
 
 	public static void main(String[] args) {
+		//xixi
 		SpringApplication.run(GiddyApplication.class, args);
 	}
 
